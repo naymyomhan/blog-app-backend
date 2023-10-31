0e0ef648748c54e773eede6c9ae873401a4b0032
Nay Myo Mhan <naymyomhan@gmail.com>
2023-10-31 07:40:59 +0700

    update

---
+++ resources/views/privacy_policy.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Privacy Polidy</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="bg-white border-gray-200  dark:bg-gray-900">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" class="flex items-center">
+              <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Apyar Sar Oak</span>
+          </a>
+          <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+              </svg>
+          </button>
+
+          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a href="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="/privacy_policy" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <header class="bg-blue-500 py-4 text-white">
+        <div class="container mx-auto">
+            <h1 class="text-2xl font-semibold">Privacy Policy</h1>
+        </div>
+    </header>
+    <main class="container mx-auto p-4">
+        <p class="mb-4">Last updated: [Insert Date]</p>
+
+        <h2 class="text-xl font-semibold mb-2">1. Introduction</h2>
+        <p>These Terms and Conditions ("Terms") govern your use of the Apyar Sar Oak (Aypar Book) application (referred to as the "App") provided by Apex Studio ("we," "us," or "our"). By using the App, you agree to abide by these Terms. Please read these Terms carefully.</p>
+
+        <h2 class="text-xl font-semibold mt-4">Acceptance of Terms</h2>
+        <p>By using the Apyar Sar Oak (Aypar Book) application, you agree to be bound by these Terms. If you do not agree to these Terms, you may not use the App.</p>
+
+        <h2 class="text-xl font-semibold mt-4">2. Use of the App</h2>
+        <p>2.1. Eligibility: You must be at least 18 years of age or have the necessary legal capacity in your jurisdiction to use the App. By using the App, you represent and warrant that you meet these eligibility requirements. 2.2. License: We grant you a limited, non-exclusive, non-transferable, and revocable license to use the App for personal and non-commercial purposes.</p>
+
+        <h2 class="text-xl font-semibold mt-4">3. Intellectual Property</h2>
+        <p>3.1. Content Ownership: All content available on the App, including but not limited to books and written materials, is the property of Apex Studio or its respective content providers and is protected by copyright and other intellectual property laws.
+
+            3.2. Restrictions: You may not copy, reproduce, distribute, or create derivative works based on the content available on the App without prior written consent from Apex Studio.</p>
+
+            <h2 class="text-xl font-semibold mt-4">4. Privacy</h2>
+            <p>4.1. Data Collection: We do not collect or store any personal information or data from the App.</p>
+
+            <h2 class="text-xl font-semibold mt-4">5. Links to Third-Party Websites</h2>
+            <p>The App may contain links to third-party websites. We are not responsible for the content or privacy practices of these websites. Your use of third-party websites is at your own risk.</p>
+
+            <h2 class="text-xl font-semibold mt-4">Disclaimers and Limitation of Liability</h2>
+            <p>6.1. No Warranty: The App is provided "as is" and "as available" without any warranties of any kind, either express or implied. We do not warrant that the App will be error-free or uninterrupted.
+
+                6.2. Limitation of Liability: To the fullest extent permitted by applicable law, Apex Studio shall not be liable for any direct, indirect, incidental, consequential, or special damages arising out of or in connection with the use or inability to use the App.</p>
+
+
+
+            <h2 class="text-xl font-semibold mt-4">7. Termination</h2>
+            <p>We reserve the right to terminate or suspend your access to the App, without notice, for any reason, including, but not limited to, a violation of these Terms.</p>
+
+
+
+
+
+            <h2 class="text-xl font-semibold mt-4">Changes to Terms</h2>
+            <p>We reserve the right to update or modify these Terms at any time. Any changes will be posted on the App, and your continued use of the App after such changes constitutes your acceptance of the modified Terms.</p>
+
+            <h2 class="text-xl font-semibold mt-4">Governing Law</h2>
+            <p>These Terms are governed by and construed in accordance with the laws of [Your Jurisdiction], without regard to its conflict of law principles.
+
+                By using the Apyar Sar Oak (Aypar Book) application, you acknowledge that you have read, understood, and agree to these Terms and Conditions.</p>
+    </main>
+</body>
+</html>
