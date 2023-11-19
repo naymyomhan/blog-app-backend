0a977185589965198e76f6c942d74056bded57b7
Nay Myo Mhan <naymyomhan@gmail.com>
2023-11-19 19:11:59 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Application</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <meta name="description" content="အပြာစာအုပ်တွေအများကြီးဖတ်ဖို့ ဒီ app လေးကိုဒေါင်းထားပါ ">
+    <meta name="keywords" content="Apyar Sar Oak , Apyar Kar, XXX, xxx, All Kar, All book , မြန်မာအိုး , မြန်မာ HD, အပြာကား , အပြာစာအုပ်">
+
+    <meta name="description" content="အပြာစာအုပ်တွေအများကြီးဖတ်ဖို့ ဒီ app လေးကိုဒေါင်းထားပါ ">
 </head>
 <body class="bg-white">
 
@@ -37,7 +41,7 @@
     <section class="dark:bg-gray-900 h-screen">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12  h-full">
             <div class="mr-auto place-self-center lg:col-span-7">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Read and Learn with Apyar Sar Oak</h1>
+                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">အပြာစာအုပ်တွေဖတ်ဖို့ ဒီAppလေး Down ထားပါ</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Read stories and take leason from Apyar Sar Oak and practice then use in real life.Hope you enjoy our application.</p>
                 <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Get started
