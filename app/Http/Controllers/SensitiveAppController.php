1d2057b1a1aede9f2b564fc902889af89f9c6007
Nay Myo Mhan <naymyomhan@gmail.com>
2023-11-21 12:35:03 +0700

    upeate

--- app/Http/Controllers/SensitiveAppController.php
+++ app/Http/Controllers/SensitiveAppController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Slider;
+use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 
 class SensitiveAppController extends Controller
 {
-    //
+    use ResponseTrait;
+    public function getSliders()
+    {
+        $sliders = Slider::where('sensitive', 1)->get();
+        foreach ($sliders as $slider) {
+            $slider->image = env('APP_URL') . "/storage/" . $slider->image;
+            unset($slider->created_at);
+            unset($slider->updated_at);
+        }
+        return $this->success("Sliders", $sliders);
+    }
 }
