ad948e48f8b014932aaee5f33304e6b164c1fd4d
ygnCybernoob <kyawgyi16.knp@gmail.com>
2023-12-09 11:29:31 +0700

    [update] new randomize feature

--- app/Http/Controllers/SensitiveBookController.php
+++ app/Http/Controllers/SensitiveBookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class SensitiveBookController extends Controller
 {
@@ -13,14 +14,14 @@ class SensitiveBookController extends Controller
     {
         $query = Book::query();
 
-        if ($request->has('order_by')) {
-            $orderParameter = $request->input('order_by');
-            if ($orderParameter == 'view') {
-                $query->orderBy('read_count', 'desc');
-            }
-        } else {
-            $query->orderBy('read_count', 'asc');
-        }
+        $currentDateTime = Carbon::now();
+
+        $timestamp = $currentDateTime->timestamp;
+
+        $totalDays = floor($timestamp / (60 * 60 * 24));
+
+
+        $query->orderByRaw("RAND($totalDays)");
 
         $books = $query->where('sensitive', 1)->paginate(20);
 
@@ -45,8 +46,11 @@ class SensitiveBookController extends Controller
 
     public function getRecommend(Request $request)
     {
-        $books = Book::where('sensitive', 1)->orderBy('read_count', 'asc')->take(10)->get();
+        $randomNumber = rand(0, 999999);
 
+        $books = Book::where('sensitive', 1)
+        ->orderByRaw("RAND($randomNumber)")->take(10)->get();
+        
         foreach ($books as $book) {
             $book->date = $book->upload_at->format('Y-m-d');
             $book->image = env('APP_URL') . "/storage/" . $book->image;
